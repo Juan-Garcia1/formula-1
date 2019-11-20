fd11fe928509c08e4ebfa8205918ff55692c1d6e
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2019-11-20 14:42:00 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+let mainPosts = document.querySelectorAll(".main-post");
+let posts = document.querySelectorAll(".post");
+
+let i = 0;
+let postIndex = 0;
+let currentPost = posts[postIndex];
+let currentMainPost = mainPosts[postIndex];
+
+let progressInterval = setInterval(progress, 100); // 180
+
+function progress() {
+  if (i === 100) {
+    i = 0;
+    // reset the current post progress bar width back to 0
+    currentPost.querySelector(".post__progress-bar").style.width = 0;
+    currentPost.classList.remove("post--active");
+
+    postIndex++;
+
+    currentMainPost.classList.add("main-post--not-active");
+    currentMainPost.classList.remove("main-post--active");
+
+    // reset postIndex to loop over the slides again
+    if (postIndex === posts.length) {
+      postIndex = 0;
+    }
+
+    currentPost = posts[postIndex];
+    currentMainPost = mainPosts[postIndex];
+  } else {
+    i++;
+    currentPost.querySelector(".post__progress-bar").style.width = `${i}%`;
+    currentPost.classList.add("post--active");
+
+    currentMainPost.classList.add("main-post--active");
+    currentMainPost.classList.remove("main-post--not-active");
+  }
+}
